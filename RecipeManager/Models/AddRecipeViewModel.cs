445be18b9650acb2b8b86e1ac9429d89b6e39501
rej8206 <rej8260@gmail.com>
2017-04-21 18:20:07 -0400

    made a recipe parts class, still workign on add recipe

--- RecipeManager/Models/AddRecipeViewModel.cs
+++ RecipeManager/Models/AddRecipeViewModel.cs
@@ -9,14 +9,13 @@ namespace RecipeManager.Models
     public class AddRecipeViewModel
     {
         public Recipe Recipe { get; set; }
-        public List<Ingredient> ingNames {get; set;}
-        //public List< PartAmount { get; set; }
-        public string measurements{ get; set; }
-        //public 
+        public RecipePart RecipePart { get; set; }
+       
 
         public AddRecipeViewModel(){
             
             
+
             }
 
     }
